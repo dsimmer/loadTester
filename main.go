a2d7f70358b1191dbd9c61d9fb0d08297dc4db77
Dylan Simmer <goldenoblivion@gmail.com>
2018-10-09 09:32:05 +1100

    Justice script

--- main.go
+++ main.go
@@ -2,12 +2,11 @@ package main
 
 import (
 	"bytes"
-	"encoding/base64"
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
+	"math/rand"
 	"net/http"
-	"net/url"
+	"os"
 	"time"
 
 	vegeta "github.com/tsenart/vegeta/lib"
@@ -32,61 +31,28 @@ func retry(err error, chann chan []byte, env string) {
 	getTarget(chann, env)
 }
 
+const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+func RandStringBytes(n int) string {
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = letterBytes[rand.Intn(len(letterBytes))]
+	}
+	return string(b)
+}
+
 // getTarget is an implementation specific, thread safe function that will create a customer and return the target format required by the attacker
 func getTarget(chann chan []byte, env string) {
 	// The target API is urlencoded, not json
-	data := url.Values{}
 	jsonTargetter := make(map[string]string)
 
-	// For some reason we need to create the user in the "dev1" domain, but target the "dev" domain
-	envString := env
-	if env == "dev1" {
-		envString = "dev"
-	}
+	cacheBuster := string(rand.Intn(100000))
+	searchTerm := "kei"
+	// searchTerm := RandStringBytes(rand.Intn(5) + 3)
 
-	if env != "local" {
-		// User API details
-		jsonDetails := make(map[string]string)
-		jsonDetails["product"] = "account"
-		jsonDetails["env"] = env
-		jsonDetails["token"] = "AutomationTest"
-		body, err := json.Marshal(jsonDetails)
-		if err != nil {
-			retry(err, chann, env)
-			return
-		}
+	jsonTargetter["url"] = "https://perf.justconnect.justice.nsw.gov.au/api/users/lookup/" + searchTerm + "?_=" + cacheBuster
 
-		rsp, err := http.Post("http://localhost:3001/createuser", "application/json", bytes.NewReader(body))
-		if err != nil {
-			retry(err, chann, env)
-			return
-		}
-
-		body_byte, err := ioutil.ReadAll(rsp.Body)
-		if err != nil {
-			retry(err, chann, env)
-			return
-		}
-		data.Set("username", string(body_byte))
-		data.Set("password", "test1234")
-		jsonTargetter["url"] = "http://" + envString + ".zip.co/login/connect/token"
-	} else {
-		data.Set("username", "sweet2@mailinator.com")
-		data.Set("password", "123456")
-		jsonTargetter["url"] = "http://localhost:5000/login/connect/token"
-	}
-
-	data.Set("client_id", "zip.ios.wallet")
-	data.Set("client_secret", "secret")
-	data.Set("grant_type", "password")
-
-	encodedData := &bytes.Buffer{}
-	encoder := base64.NewEncoder(base64.StdEncoding, encodedData)
-	defer encoder.Close()
-	encoder.Write([]byte(data.Encode()))
-
-	jsonTargetter["body"] = encodedData.String()
-	jsonTargetter["method"] = "POST"
+	jsonTargetter["method"] = "GET"
 
 	targetter, err := json.Marshal(jsonTargetter)
 	if err != nil {
@@ -100,7 +66,6 @@ func getTarget(chann chan []byte, env string) {
 // getTargets is a generic target fetcher
 func getTargets(numberOfTargets int, env string) []byte {
 	// Hitting the server with 1000 concurrent requests is a good way to have 0 successful responses
-	simultaneousConnections := 5
 	var targets []byte
 	// Create a message channel for thread communication that can hold all our uses
 	messages := make(chan []byte, numberOfTargets)
@@ -112,33 +77,22 @@ func getTargets(numberOfTargets int, env string) []byte {
 		go getTarget(messages, env)
 
 		// Pulling from a channel will block until a result is available.
-		if i > simultaneousConnections {
-			msg := <-messages
-			targets = append(targets, msg...)
-			targets = append(targets, []byte("\n")...)
-			fmt.Printf("Completed %v of %v\n", i-simultaneousConnections, numberOfTargets)
-		}
-	}
-
-	// Pull the remaining results from the channel
-	for i := 1; i <= simultaneousConnections; i++ {
 		msg := <-messages
 		targets = append(targets, msg...)
-		if i != simultaneousConnections {
-			targets = append(targets, []byte("\n")...)
-		}
-		fmt.Printf("Completed %v of %v\n", numberOfTargets-simultaneousConnections+i, numberOfTargets)
+		targets = append(targets, []byte("\n")...)
+		fmt.Printf("Completed %v of %v\n", i, numberOfTargets)
 	}
+
 	fmt.Println("Done")
 	return targets
 }
 
 func orchestrateAttack(vectors []Vector, env string) {
 	for _, vector := range vectors {
-		rate := uint64(vector.rate) // per second
+		rate := vegeta.Rate{Freq: vector.rate, Per: time.Second}
 
 		header := http.Header{}
-		header.Set("content-type", "application/x-www-form-urlencoded")
+		header.Set("Authorization", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI1Yjk4NzFjY2E5MzJmYjAwMGY3MDljYzIiLCJ1c2VyVHlwZSI6IlNVUEVSX1VTRVIiLCJ1c2VyQWdlbmN5IjoiNTlkNmI5NjI3YmNiM2U0NmZjZDk2N2Y5IiwidXNlckFnZW5jeU5hbWUiOiJKdXN0IENvbm5lY3QiLCJ1c2VyQWdlbmN5S2V5IjoiSkMiLCJ1c2VyTG9jYXRpb25zIjpbIjU5ZDZiOTYyN2JjYjNlNDZmY2Q5NjgwNiJdLCJ1c2VyUm9sZXMiOlt7Im5hbWUiOiJKdXN0IENvbm5lY3QgUHJvZHVjdCBPd25lciIsInBlcm1pc3Npb25zIjpbIio6KjoqOioiLCI1OWQ2Yjk2MjdiY2IzZTQ2ZmNkOTY3Zjk6Kjp1c2VyOnJlYWQiXSwidGFncyI6WyJDQU5fQ0FOQ0VMX1BFTkRJTkdfQVBQT0lOVE1FTlQiXSwiY3JlYXRlZERhdGUiOiIyMDE3LTEwLTA1VDIyOjU5OjUwLjA5MVoiLCJsYXN0TW9kaWZpZWREYXRlIjoiMjAxOC0wNi0wMVQwNjo1MToyOC4xOTVaIiwiaWQiOiI1OWQ2Yjk2NjdiY2IzZTQ2ZmNkOTZiZmIiLCJhZ2VuY3lJZCI6IjU5ZDZiOTYyN2JjYjNlNDZmY2Q5NjdmOSIsImxhc3RNb2RpZmllZEJ5SWQiOiI1YTI5YjEzNjA0YTU1YjAwMGY4MDdjOWMiLCJrZXkiOiJTVVBFUl9BRE1JTiJ9XSwidXNlclRhZ3MiOlsiQ0FOX0NBTkNFTF9QRU5ESU5HX0FQUE9JTlRNRU5UIl0sInByZXZpb3VzTG9naW5EYXRlIjoiMjAxOC0wOS0xNFQwNDo0MTozOS4yMTVaIiwiY3VycmVudExvZ2luRGF0ZSI6IjIwMTgtMTAtMDVUMDI6Mjg6MDMuMjY5WiIsImV4cCI6MTUzODcxMDA4MywiaWF0IjoxNTM4NzA2NDgzfQ.zaKqUzLlwhzr9e_mMFW7U8XRJLzvgh12VCvmM0LH2Ng")
 		src := getTargets(vector.numberOfTargets, env)
 
 		// Preloads all our attack data since each request is unique. Otherwise we wouldn't be able to sustain the require rate a loadtest requires
@@ -150,15 +104,26 @@ func orchestrateAttack(vectors []Vector, env string) {
 		attacker := vegeta.NewAttacker()
 
 		var metrics vegeta.Metrics
+		var (
+			rep    vegeta.Reporter
+			report vegeta.Report
+		)
+
+		rep, report = vegeta.NewTextReporter(&metrics), &metrics
 
 		fmt.Println("Started vector")
 		for res := range attacker.Attack(targeter, rate, vector.duration, "perf") {
-			metrics.Add(res)
+			//print body
+			// fmt.Println(string(res.Body))
+			report.Add(res)
 		}
-		metrics.Close()
+		if c, ok := report.(vegeta.Closer); ok {
+			c.Close()
+		}
 
 		fmt.Printf("\nVector:\nRate: %v, Duration: %v, Unique Targets: %v\n", rate, vector.duration, vector.numberOfTargets)
-		fmt.Printf("%+v\n", metrics)
+		metrics.Errors = []string{}
+		rep.Report(os.Stdout)
 	}
 	fmt.Println("Finished attack")
 }
@@ -167,7 +132,7 @@ func main() {
 	env := "local"
 	attackSession := []Vector{
 		// Vector{rate: 10, duration: time.Second, numberOfTargets: 10},
-		Vector{rate: 10, duration: 10 * time.Second, numberOfTargets: 10},
+		Vector{rate: 30, duration: 10 * time.Second, numberOfTargets: 1000},
 		// Vector{rate: 100, duration: time.Second, numberOfTargets: 10},
 		// Vector{rate: 100, duration: 5 * time.Second, numberOfTargets: 10},
 		// Vector{rate: 250, duration: time.Second, numberOfTargets: 10},
